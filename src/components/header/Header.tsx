b49733b3d40eb4246eae06f8f185edd35e7ea1c2
Roy Atali <royatali94@gmail.com>
2024-03-30 14:18:31 +0300

    fix header file

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,6 +9,7 @@ const Header: React.FC<HeaderProps> = ({ title, sm }): React.JSX.Element => {
   return (
     <header
       className={`${sm ? "text-3xl" : "text-6xl"} font-bold p-4 text-center`}
+      style={{ lineHeight: "1.4", paddingBottom: "0.5em" }}
     >
       <h1 className="bg-clip-text text-transparent bg-text-gradient">
         {title}
