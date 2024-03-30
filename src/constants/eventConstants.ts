17ca64d5c3666d1ca80b8aa7fca5f31b2e60e46e
Roy Atali <royatali94@gmail.com>
2024-03-30 20:11:27 +0300

    change post and event constants

--- src/constants/eventConstants.ts
+++ src/constants/eventConstants.ts
@@ -1,3 +1,3 @@
-export const MAX_TITLE_LENGTH: number = 50;
+export const MAX_TITLE_LENGTH: number = 150;
 export const MAX_CONTENT_LENGTH: number = 100000;
 export const MAX_TAGS_LENGTH: number = 100;
